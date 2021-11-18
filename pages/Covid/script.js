a28242b8cde875b924e461fab2d23834a7898179
GanXx8 <ganda.imasxx@gmail.com>
2021-11-18 23:46:51 +0700

    get api covid

---
+++ pages/Covid/script.js
@@ -0,0 +1,25 @@
+const formatNumber = (number) => Intl.NumberFormat('id-ID').format(number)
+
+const setIndonesiaData = (data) => {
+    const positive = document.getElementById('covid-positive')
+    const sembuh = document.getElementById('covid-sembuh')
+    const meninggal = document.getElementById('covid-meninggal')
+    const perawatan = document.getElementById('perawatan')
+    
+    positive.textContent = formatNumber(data.positif)
+    sembuh.textContent = formatNumber(data.sembuh)
+    meninggal.textContent = formatNumber(data.meninggal)
+    perawatan.textContent = formatNumber(data.dirawat)
+}
+
+const getIndonesiaData = () => {
+    try {
+        fetch('https://apicovid19indonesia-v2.vercel.app/api/indonesia')
+            .then(res => res.json())
+            .then(json => setIndonesiaData(json))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getIndonesiaData()
